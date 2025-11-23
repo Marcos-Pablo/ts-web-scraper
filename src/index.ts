f9955582b0d25db424db6ead37521f0d076377dc
Marcos Pablo <marquin.pablo@gmail.com>
2025-11-23 20:52:20 -0300

    feat(crawl): add recursive crawlPage with page limit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+import { argv } from 'node:process';
+
+function main() {
+  if (argv.length !== 3) {
+    console.error('Invalid arguments. Usage: npm run start <base_url>');
+    process.exit(1);
+  }
+
+  const baseURL = argv[2];
+
+  console.log(`Crawling starting on ${baseURL}`);
+  process.exit(0);
+}
+
+main();
