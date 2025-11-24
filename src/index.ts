e853c5429d0f1637224c9fb4faf118382f89bbbe
Marcos Pablo <marquin.pablo@gmail.com>
2025-11-23 22:04:19 -0300

    feat(fetch): add getHTML utility with error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { argv } from 'node:process';
+import { getHTML } from './crawl';
 
-function main() {
+async function main() {
   if (argv.length !== 3) {
     console.error('Invalid arguments. Usage: npm run start <base_url>');
     process.exit(1);
@@ -9,6 +10,10 @@ function main() {
   const baseURL = argv[2];
 
   console.log(`Crawling starting on ${baseURL}`);
+  const HTML = await getHTML(baseURL);
+  if (HTML) {
+    console.log(HTML);
+  }
   process.exit(0);
 }
 
